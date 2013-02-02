d2bbb871d564cdc14328878f6ad68730228a7619
Andrew Stone <andrew.j.stone.1@gmail.com>
2013-02-02 15:50:30 -0500

    make public the root

--- index.js
+++ index.js
@@ -6,12 +6,8 @@ app.configure(function() {
   app.use(express.bodyParser());
   app.use(express.cookieParser());
   app.use(app.router);
-  app.use('/public', express.static(__dirname + '/public'));
+  app.use('/', express.static(__dirname + '/public'));
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true})); 
-});
-
-app.get('/', function(req, res) {
-  res.end('Get Hyphy on Vine Bro');
 });
 
 app.listen(8000, function() {
