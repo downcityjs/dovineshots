413f0027eba7caaa3d1f52c7e54a0e32d323ef12
Andrew Stone <andrew.j.stone.1@gmail.com>
2013-02-02 15:44:35 -0500

    add index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express.createServer();
+
+app.configure(function() {
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(app.router);
+  app.use('/public', express.static(__dirname + '/public'));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true})); 
+});
+
+app.listen(8000, function() {
+  console.log('dovineshots listening on port 8000');
+});
